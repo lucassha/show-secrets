6a2af177dbebaef448391178a4ed1e28092c032d
Shannon Lucas <shannonlucas4@gmail.com>
2020-11-21 22:20:55 -0800

    add kubeconfig func testing

---
+++ kubeconfig_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"k8s.io/client-go/util/homedir"
+)
+
+func TestGetKubeconfigEnvVar(t *testing.T) {
+	// dont overwrite the kubeconfig path if it exists
+	var tmp string
+	if os.Getenv("KUBECONFIG") != "" {
+		os.Unsetenv("KUBECONFIG")
+		// re-write upon exit
+		defer os.Setenv("KUBECONFIG", tmp)
+	}
+
+	testTable := []struct {
+		name     string
+		fakePath string
+	}{
+		{name: "test0", fakePath: "~/.kube/fakeconfig"},
+		{name: "test1", fakePath: "~/.notkube/mayberealconfig"},
+	}
+
+	for _, tc := range testTable {
+		t.Run(tc.name, func(t *testing.T) {
+			os.Setenv("KUBECONFIG", tc.fakePath)
+			path := getKubeConfig()
+			assert.Equal(t, tc.fakePath, path, "they should be equal")
+		})
+	}
+}
+
+func TestKubeConfig(t *testing.T) {
+	// dont overwrite the kubeconfig path if it exists
+	var tmp string
+	if os.Getenv("KUBECONFIG") != "" {
+		os.Unsetenv("KUBECONFIG")
+		// re-write upon exit
+		defer os.Setenv("KUBECONFIG", tmp)
+	}
+
+	expectedPath := filepath.Join(homedir.HomeDir(), ".kube", "config")
+	actualPath := getKubeConfig()
+	assert.Equal(t, expectedPath, actualPath, "they should be equal")
+}
