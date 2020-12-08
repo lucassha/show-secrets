c0aea4c2b478f54898608b96bdfffdb16fe2ff19
Shannon Lucas <shannonlucas4@gmail.com>
2020-12-08 08:45:01 -0800

    add basic testing for parsing decoded secrets

---
+++ outputs_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	apiv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// TestCreateJsonObject tests struct creation of decoded kubernetes secrets
+// into a DecodedSecretList struct object
+func TestCreateDecodedObject(t *testing.T) {
+	testTable := []struct {
+		name    string
+		secrets []apiv1.Secret
+		decoded DecodedSecretList
+	}{
+		{
+			name: "test case 1",
+			secrets: []apiv1.Secret{
+				apiv1.Secret{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "test1",
+					},
+					Data: map[string][]byte{
+						"data": []byte("thisisnotrealdata"),
+					},
+				},
+			},
+			decoded: DecodedSecretList{
+				Items: 1,
+				DecodedSecrets: []DecodedSecret{
+					DecodedSecret{
+						Name: "test1",
+						Data: map[string]string{
+							"data": "thisisnotrealdata",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range testTable {
+		t.Run(tc.name, func(t *testing.T) {
+			got := createDecodedObject(tc.secrets)
+			assert.Equal(t, tc.decoded, got, "they should be equal")
+		})
+	}
+
+}
