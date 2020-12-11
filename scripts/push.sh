94298a7a0be269708278f8301961602c9651cfd3
Shannon Lucas <shannonlucas4@gmail.com>
2020-12-10 21:08:54 -0800

    testing push.sh

---
+++ scripts/push.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# push.sh will only release when a new tag is 
+# pushed to github and github actions runs the 
+# push-release.yml action
+
+LATEST_RELEASE=$(git tag --tags --abbrev=0)
+
+# build both binaries
+go build -o show-secrets ../
+go build -o kubectl-show-secrets ../
+
+# todo 
+# update 
