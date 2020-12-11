e372adf5aff15735e28852a301a44a731f6d07eb
Shannon Lucas <shannonlucas4@gmail.com>
2020-12-10 20:46:22 -0800

    add s3 infra for pushing to s3 from gh actions

---
+++ terraform/main.tf
@@ -0,0 +1,57 @@
+# this terraform creates an iam user for github actions to upload 
+# to S3 upon a new release
+# --
+# it must be run locally w/ profile "shannon"
+
+provider "aws" {
+  region  = "us-west-2"
+  profile = "shannon"
+}
+
+terraform {
+  backend "s3" {
+    bucket  = "shannon-terraform"
+    key     = "show-secrets/terraform.tfstate"
+    region  = "us-west-2"
+    profile = "shannon"
+  }
+}
+
+resource "aws_s3_bucket" "show_secrets_bucket" {
+  bucket = "lucassha-show-secrets-releases"
+  acl    = "public-read"
+
+  tags = {
+    created_by_terraform = "true"
+    owner                = "shannon"
+  }
+}
+
+resource "aws_iam_user" "s3" {
+  name = "s3_show_secrets_user"
+  path = "/system/"
+
+  tags = {
+    created_by_terraform = "true"
+    owner                = "shannon"
+  }
+}
+
+resource "aws_iam_user_policy" "s3_user_policy" {
+  name = "s3_show_secrets_user_policy"
+  user = aws_iam_user.s3.name
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "AllowAllToShannonTerraformBucket",
+      "Action": "s3:*",
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.show_secrets_bucket.bucket}"
+    }
+  ]
+}
+EOF    
+}
