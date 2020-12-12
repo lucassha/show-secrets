08e236aaabb46cdb24573efcf364a21b66926c47
Shannon Lucas <shannonlucas4@gmail.com>
2020-12-12 08:29:05 -0800

    update script

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-# push.sh will only release when a new tag is 
+# ./push.sh will only run when a new tag is 
 # pushed to github and github actions runs the 
 # push-release.yml action
 
 LATEST_RELEASE=$(git describe --tags --abbrev=0)
 
-echo "trying to find where we are in the directory bc i know nothing about github actions"
-ls -al
-pwd
+# build both binaries for mac
+GOOS=darwin GOARCH=amd64 go build -o show-secrets
+GOOS=darwin GOARCH=amd64 go build -o kubectl-show-secrets
 
-# build both binaries
-go build -o show-secrets
-go build -o kubectl-show-secrets
-
-echo "WE MADE IT THIS FAR"
+tar -czvf showsecrets.tar.gz ./kubectl-show-secrets ./show-secrets
 
 # todo 
-# update 
+# push up to S3
