6370a399f2e22f3a61dcbe4db3381cd166d4afa7
Shannon Lucas <shannonlucas4@gmail.com>
2020-12-13 09:50:05 -0800

    vars update

--- scripts/push.sh
+++ scripts/push.sh
@@ -5,10 +5,12 @@
 # push-release.yml action
 
 LATEST_RELEASE=$(git describe --tags --abbrev=0)
+OS="darwin"
+ARCH="amd64"
 
 # build both binaries for mac
-GOOS=darwin GOARCH=amd64 go build -o show-secrets
-GOOS=darwin GOARCH=amd64 go build -o kubectl-show-secrets
+GOOS=${OS} GOARCH=${ARCH} go build -o show-secrets
+GOOS=${OS} GOARCH=${ARCH} go build -o kubectl-show-secrets
 
 tar -czvf showsecrets.tar.gz ./kubectl-show-secrets ./show-secrets
 
