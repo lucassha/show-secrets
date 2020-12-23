6a112c7e3fff6347dae48eb4401058e41d4a1a55
Shannon Lucas <shannonlucas4@gmail.com>
2020-12-22 19:06:54 -0800

    push tarball release to s3

--- scripts/push.sh
+++ scripts/push.sh
@@ -4,15 +4,19 @@
 # pushed to github and github actions runs the 
 # push-release.yml action
 
-LATEST_RELEASE=$(git describe --tags --abbrev=0)
+set -eu
+
+S3_TARBALL="showsecrets.tar.gz"
+S3_BUCKET="lucassha-show-secrets-releases"
 OS="darwin"
 ARCH="amd64"
+
+LATEST_RELEASE=$(git describe --tags --abbrev=0)
 
 # build both binaries for mac
 GOOS=${OS} GOARCH=${ARCH} go build -o show-secrets
 GOOS=${OS} GOARCH=${ARCH} go build -o kubectl-show-secrets
 
-tar -czvf showsecrets.tar.gz ./kubectl-show-secrets ./show-secrets
+tar -czvf ${S3_TARBALL}-${LATEST_RELEASE} ./kubectl-show-secrets ./show-secrets
 
-# todo 
-# push up to S3
+aws s3 cp ${S3_TARBALL}-${LATEST_RELEASE} s3://${S3_BUCKET}/releases/${LATEST_RELEASE} --profile shannon
